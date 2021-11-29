e7c4dfc15de1447901a8e0a8341a6847b12514bb
Vagner Urata <vagnerurata@gmail.com>
2021-11-29 17:43:05 -0300

    Adicionado cadastro

--- src/main/java/br/com/primeiroprojetospring/controller/ProfessorController.java
+++ src/main/java/br/com/primeiroprojetospring/controller/ProfessorController.java
@@ -3,9 +3,11 @@ package br.com.primeiroprojetospring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.primeiroprojetospring.domain.Professor;
 import br.com.primeiroprojetospring.service.ProfessorService;
 
 @Controller
@@ -21,5 +23,18 @@ public class ProfessorController {
 		mView.addObject("professores", professorService.buscarTodosProfessores());
 		return mView;
 
+	}
+
+	@GetMapping("/cadastrar")
+	public ModelAndView cadastrarProfessor() {
+		ModelAndView mView = new ModelAndView("professor/cadastraProfessor");
+		mView.addObject("professor", new Professor());
+		return mView;
+	}
+
+	@PostMapping("/salvar")
+	public ModelAndView salvarProfessor(Professor professor) {
+		professorService.salvar(professor);
+		return listaTodosProfessores();
 	}
 }
